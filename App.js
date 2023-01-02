47c5427567df2c6008327553f0b5d4e00e4afe5d
bibhuprasadrout <bibhu.rs@gmail.com>
2023-01-03 01:49:53 +0530

    Set up react as dependency and parcel as dev-dependency for the project. Add .gitIgnore file for the project.

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+export default App = () => {
+  return <h1>Track Expenses Here!</h1>;
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
