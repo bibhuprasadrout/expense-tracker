eb567b7cff8d764147dc02a10fbeabf4104f75a7
bibhuprasadrout <bibhu.rs@gmail.com>
2023-03-11 15:45:55 +0530

    Fix deployment issue in netlify

--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -4,17 +4,16 @@ import { useEffect, useState } from "react";
 import { Wrapper } from "../../DesignSystem/Wrapper/Wrapper";
 import { Card } from "../../DesignSystem/Card/Card";
 export const Home = () => {
-  const homeStatus = useSelector((store) => store.userAuth);
-  useEffect(() => {}, []);
+  const homeAuth = useSelector((store) => store.userAuth);
+  const [homeStatus, setHomeStatus] = useState(false);
+  useEffect(() => {
+    homeAuth?.currentLoginState ? setHomeStatus(true) : setHomeStatus(false);
+  }, []);
   return (
     <>
       <Wrapper>
         <Card>
-          {homeStatus?.currentLoginState ? (
-            <ExpenseTracker />
-          ) : (
-            <div>Please login first</div>
-          )}
+          {homeStatus ? <ExpenseTracker /> : <div>Please login first</div>}
         </Card>
       </Wrapper>
     </>
