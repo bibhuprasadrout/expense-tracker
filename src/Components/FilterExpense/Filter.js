982fdfc63bde173d1800902263e96ca30b3cc69a
bibhuprasadrout <bibhu.rs@gmail.com>
2023-03-24 05:41:12 +0530

    Fix bug in filter.js

--- src/Components/FilterExpense/Filter.js
+++ src/Components/FilterExpense/Filter.js
@@ -193,8 +193,8 @@ export const Filter = ({ expenseState, setExpenseState }) => {
     "December",
   ];
   const days = [
-    01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   ];
   const handleYearOption = () => {
     setOptionYear((prev) => !prev);
